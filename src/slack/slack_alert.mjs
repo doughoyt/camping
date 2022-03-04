fad2ffa816dcb50f51fa648aee01ab0d1efe4479
Doug Hoyt <doughoyt@gmail.com>
2022-03-04 08:26:38 -0700

    Add plain-text slack alert method

--- src/slack/slack_alert.mjs
+++ src/slack/slack_alert.mjs
@@ -3,11 +3,19 @@ import axios from 'axios';
 const channel_id = process.env.SLACK_CHANNEL_ID;
 const slackToken = process.env.SLACK_BOT_TOKEN;
 
-export default async function alert(message) {
+export async function alertBlock(message) {
     const url = 'https://slack.com/api/chat.postMessage';
     const res = await axios.post(url, {
       channel: channel_id,
       text: "New Alert!",
       blocks: JSON.stringify(message)
     }, { headers: { authorization: `Bearer ${slackToken}` } });
+}
+
+export async function alertText(message) {
+  const url = 'https://slack.com/api/chat.postMessage';
+  const res = await axios.post(url, {
+    channel: channel_id,
+    text: message
+  }, { headers: { authorization: `Bearer ${slackToken}` } });
 }
